022341190bde8844f1771892295625fc475a5f67
BikoPython <stevennyandoya@gmail.com>
2021-05-18 13:13:48 +0300

    recursion:direct-recursion

---
+++ recursion-recursive-functions/direct-recursion.py
@@ -0,0 +1,48 @@
+# # Print numbers using recursion:
+# def printnum(lr, ur):
+#     if lr > ur:
+#         return
+#     print(lr)
+#     printnum(lr + 1, ur); #recursive call
+#
+# n = int(input("Enter number:"))
+# printnum(1, n)
+#
+# # Printing pyramid pattern
+#
+# def printn(num):
+#     if (num == 0):
+#         return
+#     print("*", end=" ")
+#     printn(num - 1)                 #recursive call to printn function
+#
+# def pattern(n, i):
+#     if (n == 0):
+#         return
+#     printn(i)
+#     print("\n", end="")
+#     pattern(n - 1, i + 1)           #recursive call to pattern
+#
+# n = int(input("Enter number:"))
+# pattern(n, 1)
+
+# Print Fibonacci number:
+
+def Fibonacci(n):
+    if n < 0:
+        print("Incorrect input")
+
+    elif n == 0:
+        return 0 #base case
+
+    elif n == 1 or n == 2:
+        return 1 #base case
+
+    else:
+        return Fibonacci(n - 1) + Fibonacci(n - 2) #recursive call to the function
+
+n = int(input("Enter number of terms: "))
+
+i = 0
+for i in range(n):
+    print(Fibonacci(i), end="\t")
