a8f58c9060a1f425439eb4e2345c9d4580fa2d50
BikoPython <stevennyandoya@gmail.com>
2021-05-22 15:13:01 +0300

    generators:data-pipelines-expressions

--- generators/-functions.py
+++ generators/-functions.py
@@ -59,3 +59,69 @@ for x in fibo():
     if x > 50:
         break
     print(x, end=" ")
+
+# Generating the first 10 perfect squares starting from 1:
+def perfect_square():
+    num = 1
+    while(num <= 10):
+        yield (num * num)
+        num += 1
+squares = perfect_square()
+squares
+
+next(squares)
+next(squares)
+
+# for-loop also accepts a generator:
+
+for square in perfect_squares():
+    print(square)
+
+def cookies(value):
+    while True:
+        yield value
+
+#Printing  a list of cookies
+
+def print_cookies(cookies):
+    length = len(cookies)
+    for cookie in range(0, length):
+        yield cookies[cookie]
+
+cookie_list = ["Raspberry", "Choc-Chip", "Cinnamon", "Oat"]
+
+for c in print_cookies(cookie_list):
+	print(c)
+
+# Totaling up a list of order prices:
+
+order_list = [2.30, 2.50, 1.95, 6.00, 7.50, 2.15]
+
+def total_orders(orders):
+    length = len(orders)
+    total = 0
+    for o in range(0, length):
+        total += orders[o]
+        yield total
+
+for order in total_orders(order_list):
+    print(order)
+
+# Reading Large Files:
+def csv_reader(file_name):
+    for row in open(file_name, "r"):
+        yield row
+
+# Generating an Infinite Sequence:
+
+def infinite_sequence():
+    num = 0
+    while True:
+        yield num
+        num += 1
+
+for i in infinite_sequence():
+    print(i, end=" ")
+
+
+
--- generators/generator-expressions.py
+++ generators/generator-expressions.py
@@ -17,3 +17,33 @@ print("Generator expression", end=':n')
 c = (x+2 for x in a)
 print(c)
 print(min(c))
+
+cookie_list = ["Raspberry", "Choc-Chip", "Cinnamon", "Oat"]
+
+cookie_generator = (cookie for cookie in cookie_list)
+
+for cookie in cookie_generator:
+    print(cookie)
+
+# Reading Large Files:
+csv_gen = (row for row in open(file_name))
+
+nums_squared_gc = (num**2  for num in range(5))
+
+# Creating Data Pipelines With Generators:
+
+file_name = "techcrunch.csv"
+lines = (line for line in open(file_name))
+list_line = (s.rstrip().split(",") for s in lines)
+cols = next(list_line)
+
+company_dicts = (dict(zip(cols, data)) for data in list_line)
+
+funding = (
+    int(company_dict["raisedAmt"])
+    for company_dict in company_dicts
+    if company_dict["round"] == "a"
+)
+
+total_series_a = sum(funding)
+print(f"Total series A fundraising: Ksh.{total_series_a}")
