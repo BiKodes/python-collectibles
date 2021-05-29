4f859197ab253cf959307d0318cb4cf2d5a62eb4
BikoPython <stevennyandoya@gmail.com>
2021-05-29 23:25:33 +0300

    dict-comprehension:nested

--- comprehensions/dictionaries/conditional-dictionaries.py
+++ comprehensions/dictionaries/conditional-dictionaries.py
@@ -74,3 +74,32 @@ dict2 = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f':6}
 
 dict2_tripleCond = {k:v for (k, v) in dict2.items() if v>2 if v%2 == 0 if v%3 == 0}
 print(dict2_tripleCond)
+
+# If-Else Conditions:
+dict1 = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f':6}
+
+# Identify odd and even entries:
+dict1_tripleCond = {k: ('even' if v%2 == 0 else 'odd') for (k,v) in dict1.items()}
+
+print(dict1_tripleCond)
+
+# Nested Dictionary Comprehension:
+nested_dict = {'first': {'a': 1}, 'second': {'b': 2}}
+float_dict = {outer_k: {float(inner_v) for (inner_k, inner_v) in outer_v.items()}
+              for (outer_k, outer_v) in nested_dict.items()}
+
+print(float_dict)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
